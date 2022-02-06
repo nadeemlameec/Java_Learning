e9dca0866703253a8a562c792f03128613905e62
Nadeem <nadeemlameec@gmail.com>
2022-02-06 20:42:27 +0530

    07/01/2022

---
+++ String/src/String_Intro.java
@@ -0,0 +1,14 @@
+public class String_Intro {
+    public static void main(String[] args) {
+        String a ="Nadeem";
+        String b ="Nadeem";
+
+        if (a==b){
+            System.out.println("Same String");
+        }
+        else{
+            System.out.println("Not Matched");
+        }
+
+    }
+}
